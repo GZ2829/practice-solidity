6660bea1dc9fdc715b29a678c1ec3acd2c0f9c0b
gz2829 <gzucchero24@gmail.com>
2023-09-04 15:29:22 -0400

    add openzep

---
+++ practice.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Test is Ownable, ReentrancyGuard {
+  using SafeERC20 for IERC20;
+}
